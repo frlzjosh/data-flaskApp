a47b16533e5b442c1c8d05e8f4af422a129708b8
DIS JOSH <disjosh@diss-mbp.lan>
2019-08-12 22:27:38 -0700

    init vue app

---
+++ data-client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <read-data></read-data>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import readData from '@/components/readData.vue'
+
+export default {
+  name: 'home',
+  components: {
+    readData
+  }
+}
+</script>
